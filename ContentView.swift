ea22ff2451905eb3bfdc05ffcc5d2a74c55622c6
bizzybits <ponce.emc@gmail.com>
2022-10-01 15:11:09 -0700

    added sizing for cards and scroll view and grid view

--- ContentView.swift
+++ ContentView.swift
@@ -8,47 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🍍","🫐","🍒","🍌","🍓","🍅","🍏","🍋","🍇","🍊","🍉","🍑","🥝","🥑","🌽","🥕"]
-    @State var emojiCount = 5
+    var emojis = ["🍍","🫐","🍒","🍌","🍓","🍅","🍏","🍋","🍇","🍊","🍉","🍑","🥝","🥑","🌽","🥕", "🍎", "🍐","🍈","🥥","🥦","🥬","🌶","🥒"]
+    @State var emojiCount = 20
     
     
     var body: some View {
-        VStack{
-            HStack {
-                ForEach(emojis[0..<emojiCount], id: \.self){emoji in
-                    CardView(content: emoji)
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
             }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            Spacer(minLength: 20)
             HStack {
                 remove
                 Spacer()
                 add
             }
+            .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
+    
     var remove: some View {
-        Button(action: {
+        Button {
+            if emojiCount > 1{
             emojiCount -= 1
-        }, label: {
-            VStack {
-                Text("Remove")
-                Text("Card")
             }
-        })
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
     
     var add : some View {
-        Button(action: {
+        Button {
+            if emojiCount < emojis.count {
             emojiCount += 1
-        }, label: {
-            VStack {
-                Text("Add")
-                Text("Card")
             }
-        })
+        } label: {
+            Image(systemName: "plus.circle")
+    
+        }
     }
 }
 
@@ -62,7 +67,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
@@ -92,7 +97,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.landscapeLeft)
         ContentView()
             .preferredColorScheme(.light)
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
